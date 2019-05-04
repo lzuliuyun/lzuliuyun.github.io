9f4b7cd3ff181a3bf3779fbc8181f5b2aa12bcd2
lzuliuyun <lzuliuyun@gmail.com>
2019-05-04 19:02:52 +0800

    Site updated: 2019-05-04 19:02:17

--- js/page/archive.js
+++ js/page/archive.js
@@ -1,1 +1,24 @@
-!function(){function e(e){return Array.prototype.slice.call(e)}var n=e(document.getElementById("mb-main-nav").querySelectorAll(".nav__item")),i=e(document.querySelectorAll(".page__posts"));n.forEach(function(e){e.addEventListener("click",function(e){var t=Number(this.getAttribute("data-index"));this.classList.contains("nav__item--selected")||(n.forEach(function(e){e.classList.remove("nav__item--selected")}),i.forEach(function(e){e.classList.add("js-hidden")}),n[t].classList.add("nav__item--selected"),i[t].classList.remove("js-hidden"),setTimeout(window._skappPostAnimation))})})}();
+(function() {
+    function arrayFrom(arr) {
+        return Array.prototype.slice.call(arr);
+    }
+
+    var
+        navs = arrayFrom(document.getElementById('mb-main-nav').querySelectorAll('.nav__item')),
+        postBoxs = arrayFrom(document.querySelectorAll('.page__posts'));
+
+    navs.forEach(function(nav) {
+        nav.addEventListener('click', function(event) {
+            var idx = Number(this.getAttribute('data-index'));
+
+            if (this.classList.contains('nav__item--selected')) return;
+
+            navs.forEach(function(item) { item.classList.remove('nav__item--selected') });
+            postBoxs.forEach(function(postBox) { postBox.classList.add('js-hidden') });
+
+            navs[idx].classList.add('nav__item--selected');
+            postBoxs[idx].classList.remove('js-hidden');
+            setTimeout(window._skappPostAnimation);
+        })
+    });
+})();
